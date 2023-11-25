47d12efd231ac9bcb69b6206897651253fbb7f22
Roohallah Azizi <roohallah.azizi@virtudev.net>
2023-11-25 14:25:30 +0300

    refactor: use hashset for checking vacation dates in IsTollFreeDate method

--- Congestion.Tax.Calculator.Tests/CongestionTaxCalculatorTests.cs
+++ Congestion.Tax.Calculator.Tests/CongestionTaxCalculatorTests.cs
@@ -1,11 +1,39 @@
+using System.Collections;
 using Xunit;
 
 namespace Congestion.Tax.Calculator.Tests;
 
 public class CongestionTaxCalculatorTests
 {
-    [Fact]
-    public void Test1()
+    [Theory]
+    [MemberData(nameof(CustomTestData))]
+    public void IsTollFreeDate(DateTime dateTime)
     {
+        var sut = new CongestionTaxCalculator();
+
+        var result = sut.IsTollFreeDate(dateTime);
+
+        Assert.True(result);
     }
+
+    public static IEnumerable<object[]> CustomTestData =>
+        new List<object[]>
+        {
+            new Object[] { new DateTime(2013, 1, 1) },
+            new Object[] { new DateTime(2013, 3, 28) },
+            new Object[] { new DateTime(2013, 3, 29) },
+            new Object[] { new DateTime(2013, 4, 1) },
+            new Object[] { new DateTime(2013, 4, 30) },
+            new Object[] { new DateTime(2013, 5, 1) },
+            new Object[] { new DateTime(2013, 5, 8) },
+            new Object[] { new DateTime(2013, 5, 9) },
+            new Object[] { new DateTime(2013, 6, 5) },
+            new Object[] { new DateTime(2013, 6, 6) },
+            new Object[] { new DateTime(2013, 6, 21) },
+            new Object[] { new DateTime(2013, 11, 1) },
+            new Object[] { new DateTime(2013, 12, 24) },
+            new Object[] { new DateTime(2013, 12, 25) },
+            new Object[] { new DateTime(2013, 12, 26) },
+            new Object[] { new DateTime(2013, 12, 31) }
+        };
 }
--- Congestion.Tax.Calculator/CongestionTaxCalculator.cs
+++ Congestion.Tax.Calculator/CongestionTaxCalculator.cs
@@ -62,30 +62,18 @@ public class CongestionTaxCalculator
         else return 0;
     }
 
-    private Boolean IsTollFreeDate(DateTime date)
+    public bool IsTollFreeDate(DateTime date)
     {
-        int year = date.Year;
-        int month = date.Month;
-        int day = date.Day;
+        if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday || date.Month == 7) return true;
 
-        if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday) return true;
-
-        if (year == 2013)
+        var vacations = new HashSet<DateTime>
         {
-            if (month == 1 && day == 1 ||
-                month == 3 && (day == 28 || day == 29) ||
-                month == 4 && (day == 1 || day == 30) ||
-                month == 5 && (day == 1 || day == 8 || day == 9) ||
-                month == 6 && (day == 5 || day == 6 || day == 21) ||
-                month == 7 ||
-                month == 11 && day == 1 ||
-                month == 12 && (day == 24 || day == 25 || day == 26 || day == 31))
-            {
-                return true;
-            }
-        }
-
-        return false;
+            new(2013, 1, 1), new(2013, 3, 28), new(2013, 3, 29), new(2013, 4, 1),
+            new(2013, 4, 30), new(2013, 5, 1), new(2013, 5, 8), new(2013, 5, 9),
+            new(2013, 6, 5), new(2013, 6, 6), new(2013, 6, 21), new(2013, 11, 1),
+            new(2013, 12, 24), new(2013, 12, 25), new(2013, 12, 26), new(2013, 12, 31)
+        };
+        return vacations.Contains(date);
     }
 
     private enum TollFreeVehicles
